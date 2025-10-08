1ac1fd79d92e4e3bef05950ed24ef62a10f518b8
Andres Felipe Saumet <andressaumet@gmail.com>
2025-10-08 06:55:43 -0500

    Sitemap updated

--- app/(frontend)/blog/page.tsx
+++ app/(frontend)/blog/page.tsx
@@ -41,7 +41,7 @@ export default function Blog() {
       <div className="w-9/12 max-[920px]:w-full mr-6 max-[920px]:mr-none">
         <header className="pt-4 mb-8 px-10">
           <h1 className="text-left max-[920px]:text-center">Explora la Innovación: Blog de Desarrollo Web y Móvil</h1>
-          <p className="text-justify mt-6 max-[640px]:text-pretty">Bienvenido al blog de Andres Saumet Desarrollador Web & movíl, donde comparto mis conocimientos y experiencias como desarrollador web y móvil especializado en JavaScript, TypeScript, React, Dart y Flutter. Aquí encontrarás tutoriales, guías, estudios de caso, y las últimas tendencias en tecnología, diseñados para ayudarte a mejorar tus habilidades y mantenerse al día en el mundo del desarrollo de software. ¡Únete a nuestra comunidad y lleva tu carrera al siguiente nivel!</p>
+          <p className="text-justify mt-6 max-[640px]:text-pretty">Bienvenido a mi blog de Andres Saumet Desarrollador Web & movíl, donde comparto mis conocimientos y experiencias como desarrollador web y móvil especializado en JavaScript, TypeScript, React, Dart y Flutter. Aquí encontrarás tutoriales, guías, estudios de caso, y las últimas tendencias en tecnología, diseñados para ayudarte a mejorar tus habilidades y mantenerse al día en el mundo del desarrollo de software. ¡Únete a nuestra comunidad y lleva tu carrera al siguiente nivel!</p>
         </header>
         <main>
           <CardPost />
